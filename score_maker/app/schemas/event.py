b9d6f0ae0268ffa1986bd8a21e913a8db66565d5
vladislav <vladyan198@gmail.com>
2024-10-02 12:35:22 +0300

    score_maker.proto for changing score and Schemas for API-s

---
+++ score_maker/app/schemas/event.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from enum import Enum
+
+from pydantic import BaseModel, Field
+
+from score_maker.pb.line_provider_pb2 import StatusEnum as _StatusEnum
+
+
+# Перевод Protobuf Enum в Python Enum
+class StatusEnum(Enum):
+    PROCESSING = _StatusEnum.STATUS_PROCESSING
+    HIGH_SCORE = _StatusEnum.STATUS_HIGH_SCORE
+    LOW_SCORE = _StatusEnum.STATUS_LOW_SCORE
+
+
+class BaseEvent(BaseModel):
+    event_id: int
+
+
+class EventResponse(BaseEvent):
+    deadline: datetime
+    status: StatusEnum = 0
+
---
+++ score_maker/app/schemas/score.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+from pydantic import BaseModel, Field
+
+
+class ScoreStatusEnum(Enum):
+    mutable = "mutable"
+    immutable = "immutable"
+
+
+class Score(BaseModel):
+    event_id: int
+    rate: int = Field(ge=1, le=5)
+
+
+class ScoreWithStats(Score):
+    status: ScoreStatusEnum
+
+
+class SetScoreRequest(ScoreWithStats):
+    ...
