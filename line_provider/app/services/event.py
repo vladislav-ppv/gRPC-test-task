add98fb7b9ea978f0491b5bfafe4e755a92beb7a
uiolo <vladyan198@gmail.com>
2024-10-07 12:16:54 +0300

    Real logic for fetching all events from database

--- line_provider/app/services/event.py
+++ line_provider/app/services/event.py
@@ -1,15 +1,26 @@
-import datetime
+import logging
 
+from line_provider.app.database.core import SessionLocal
+from line_provider.app.database.models import EventModel
+from line_provider.app.database.repository.event import EventRepository
+from line_provider.app.services.exceptions import ScoreIsNotBetween1And5
+from line_provider.pb.line_provider_pb2 import GetEventsResponse, GetEventsRequest, Event, \
+    UpdateEventStatusByScoreRequest, UpdateEventStatusByScoreResponse
 from line_provider.pb.line_provider_pb2_grpc import EventServiceServicer
-from line_provider.pb.line_provider_pb2 import GetEventsResponse, Event, StatusEnum
 
 
 class BaseEventServicer(EventServiceServicer):
-    def GetEvents(self, request, context):
-        # Логика для получения доступных событий
-        events = []
-        for i in range(5):
-            events.append(Event(event_id=i, deadline=datetime.datetime.now(), status=StatusEnum.STATUS_PROCESSING))
-        print(f"Getting events {request}")
+    async def GetEvents(self, request: GetEventsRequest, context):
+        """Fetch events from database and format them to the gRPC response model"""
+        async with SessionLocal() as session:
+            repo = EventRepository(session=session)
+            event_models: list[EventModel] = await repo.get_all_events()
+            event_models_for_response: list[Event | None] = []
+
+            for event in event_models:
+                event_models_for_response.append(
+                    Event(event_id=event.id, deadline=event.deadline, status=event.status.value))
 
         return GetEventsResponse(events=events)
+        return GetEventsResponse(events=event_models_for_response)
+
