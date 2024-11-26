723c19b2bdb721c890b2ca792c4b8b52f8b06ca7
vladislav <vladislav.ppv.en@gmail.com>
2024-11-26 14:48:34 +0300

    redundant cron service + docker container removed

--- arq_cron/src/database/core.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-
-from src.config import settings
-
-SQLALCHEMY_DATABASE_URL = settings.postgres.build_url()
-
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
-async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
